aee0f7325ff2226b116ed29b33412be10e685cfb
Carlos <carlosroltjr@gmail.com>
2023-10-04 13:52:49 -0300

    adding env vars

--- services/passport.js
+++ services/passport.js
@@ -1,13 +1,12 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
-const keys = require('../config/keys')
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      clientID: process.env.GOOGLE_CLIENTE_ID,
+      clientSecret: process.env.GOOGLE_CLIENTE_SECRET,
+      callbackURL: process.env.CALLBACK_URL
     },
     (accessToken, refreshToken, profile, done) => {
       console.log('access token: ', accessToken)
