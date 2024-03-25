8a3a80c205a637a5d2665b514bcf21eee4bae5b8
Carlos Junior <carlos.junior@bry.com.br>
2024-03-24 21:09:54 -0300

    Handling survey process creating survey and recipient collections creating survey routes and credits middlewe

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const keys = require('./config/keys')
 require('./models/User')
+require('./models/Survey')
 require('./services/passport')
 
 const app = express()
@@ -22,6 +23,7 @@ app.use(passport.session())
 
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
+require('./routes/suveyRoutes')(app)
 
 mongoose.connect(keys.mongoURI)
 
