c71a5d077fb36b2898a1d6d98e76538db870d060
Carlos <carlosroltjr@gmail.com>
2024-04-07 18:12:50 -0300

    send email function

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,21 +1,36 @@
 const requireLogin = require('../middlewares/requireLogin')
 const requireCredits = require('../middlewares/requireCredits')
+const mongoose = require('mongoose')
+const Survey = mongoose.model('surveys')
 const surveyTemplate = require('../services/emailTemplate/surveyTemplate')
 
 module.exports = (app, resend) => {
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
-    const { from, to, subject, body } = req.body
-    const { data, error } = await resend.emails.send({
-      from,
-      to,
+    const { title, from, recipients, subject, body } = req.body
+    const survey = new Survey({
+      title,
       subject,
-      html: surveyTemplate(body)
+      body,
+      recipients: recipients
+        .split(',')
+        .map((email) => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now()
     })
+    try {
+      const { data } = await resend.emails.send({
+        from,
+        to: recipients,
+        subject,
+        html: surveyTemplate(body)
+      })
 
-    if (error) {
+      await survey.save()
+      req.user.credits -= 1
+      const user = await req.user.save()
+      res.status(200).json({ user, data })
+    } catch (error) {
       return res.status(400).json({ error })
     }
-
-    res.status(200).json({ data })
   })
 }
